173cbc09bbdaf13ee819c7e6f4925fa82fc74674
esraa <esraa2023112@gmail.com>
2024-01-04 16:15:09 +0200

    Done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    n = dir(hidden_4)
-    for x in n:
-        if x[:2] != "__":
-            print(x)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
