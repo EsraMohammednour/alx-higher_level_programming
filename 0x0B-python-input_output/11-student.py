eb02dd9eb5aa619f8162aa27a55c08c33001825a
esra <esraa2023112@gmail.com>
2024-02-06 10:02:03 -0800

    done

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-# 11-student.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a class Student."""
+"""
+saroo
+"""
 
 
 class Student:
-    """Represent a student."""
+    """class"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Get a dictionary representation of the Student."""
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """ json"""
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+            d_list = {}
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+        return obj
+
+    def reload_from_json(self, json):
+        """reload"""
+        for attr in json:
+            self.__dict__[attr] = json[attr]
