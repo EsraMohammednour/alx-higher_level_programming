b725c95026cc50c3ef6a25a62078ac351fade3b0
esra <esraa2023112@gmail.com>
2024-02-06 16:24:28 +0300

    done

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+saroo
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    whew
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
