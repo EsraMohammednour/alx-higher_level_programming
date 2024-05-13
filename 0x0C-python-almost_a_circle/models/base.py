11de0baf25c4d81e6cf5a1b1c4dbf29e5fc7fa11
esraa <esraa2023112@gmail.com>
2024-05-13 15:10:26 +0300

    Done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,6 +21,7 @@ class Base:
         Args:
             - id: id of the instance
         """
+
         if type(id) != int and id is not None:
             raise TypeError("id must be integer")
         if id is not None:
