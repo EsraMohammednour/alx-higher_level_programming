d4635bb329b003f5635bd52253de196edbdc280a
esraa <esraa2023112@gmail.com>
2024-05-22 21:49:30 +0300

    Done

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,6 @@
 --  creates the database hbtn_0d_usa and the table cities
+CREATE database IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE cities(
 id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
 state_id INT NOT NULL,
