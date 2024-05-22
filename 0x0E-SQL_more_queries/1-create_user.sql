513ee3ca1901075efd23c5eb4a1704a9c323290a
esraa <esraa2023112@gmail.com>
2024-05-22 19:44:48 +0300

    Done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- script that creates the MySQL server user user_0d_1
-CEARTE USER 'user_0d_1'@'localhost' IDENTIFIED WITH BY 'user_0d_1_pwd';
+CEARTE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH BY 'user_0d_1_pwd';
