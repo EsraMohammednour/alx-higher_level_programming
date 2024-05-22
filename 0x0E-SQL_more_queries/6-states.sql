2cf6d00fedc37ca956bf9fd40c02223f8f2e7c8a
esraa <esraa2023112@gmail.com>
2024-05-22 20:56:36 +0300

    Done

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE hbtn_0d_usa;
 CREATE TABLE hbtn_0d_usa(
-id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-name VARCHAR(256) NOT NULL
+id INT UNIQUE NOT NULL AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id)
 );
