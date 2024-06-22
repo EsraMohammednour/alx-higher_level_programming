7e73024b080f12159216480729c3d5332f894634
esraa <esraa2023112@gmail.com>
2024-06-22 21:35:11 +0300

    done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,9 +3,8 @@
 database `hbtn_0e_0_usa`
 """
 
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
