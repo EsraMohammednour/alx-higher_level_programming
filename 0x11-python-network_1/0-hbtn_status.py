5b644e4e7fe2539cf5a06945cc85a9106956d2f6
esraa <esraa2023112@gmail.com>
2024-07-05 16:21:53 +0300

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-'''Python script that fetches https://alx-intranet.hbtn.io/status'''
-import urllib.request
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as a:
-        con = a.read()
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
-        print(f"    - type: {type(con)}")
-        print(f"    - content: {con}")
-        print(f"    - utf8 content: {con.decode('utf-8')}")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
