bf47a448d0923d212c0ba308cf450fb60cec4876
esraa <esraa2023112@gmail.com>
2024-07-05 16:04:57 +0300

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,9 @@
 import urllib.request
 
 if __name__ == "__main__":
-     u = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen(u) as a:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as a:
         con = a.read()
         print("Body response:")
-        print(f"\t - type: {type(con)}")
-        print(f"\t - content: {con}")
-        print(f"\t - utf8 content: {con.decode('utf-8')}")
+        print(f"    - type: {type(con)}")
+        print(f"    - content: {con}")
+        print(f"    - utf8 content: {con.decode('utf-8')}")
