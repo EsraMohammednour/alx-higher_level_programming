6b73160bd2d5c6a35cad4502960e568e31e61900
esraa <esraa2023112@gmail.com>
2024-07-05 19:56:50 +0300

    done

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    a = request(url)
+    a = requests.get(url)
     if (a < 400):
         print(a.text)
     print('Error code:', a.status_code)
