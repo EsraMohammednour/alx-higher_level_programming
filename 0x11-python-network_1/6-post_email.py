0178b13e908a1cdb5b7f3c23efb6c1b057630d90
esraa <esraa2023112@gmail.com>
2024-07-05 19:19:07 +0300

    done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,9 +2,8 @@
 """sends a request to the URL and displays the value of the X-Request-Id"""
 import sys
 import requests
-import urllib.parse
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {'email': sys.argv[2]}
     r = requests.post(url, data=value)
-    print(a.text)
+    print(r.text)
