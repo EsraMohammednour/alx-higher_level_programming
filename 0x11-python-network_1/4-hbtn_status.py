35c559c6949c3657f9c1e44aee4ba3e325904a9e
esraa <esraa2023112@gmail.com>
2024-07-05 18:59:39 +0300

    done

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@
 import requests
 
 if __name__ == '__main__':
-    con = requests('https://alx-intranet.hbtn.io/status')
+    con = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(con.text)))
     print("\t- content: {}".format(con.text))
